fcd3dfb99a392604ab85544ddbb044758f11ce2e
Alexandre Grison <a.grison@gmail.com>
2016-06-24 09:56:48 +0200

    simple app run as standalone jar

--- project.clj
+++ project.clj
@@ -1,16 +1,15 @@
-(defproject hello-clj-clever-cloud "0.1.0-SNAPSHOT"
+(defproject app "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.9.0-alpha5"]
                  [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]
-                 [leiningen "2.6.1"]]
+                 [ring/ring-jetty-adapter "1.5.0"]
+                 [ring "1.5.0"]]
   :plugins [[lein-ring "0.9.7"]]
-  :ring {:handler hello-clj-clever-cloud.handler/app}
+  :ring {:handler app.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.0"]]}}
-  :aot :all
-  :source-paths ["src/main/clojure"]
-  :test-paths ["src/test/clojure"])
+  :main app.handler)
--- src/app/handler.clj
+++ src/app/handler.clj
@@ -1,11 +1,17 @@
-(ns hello-clj-clever-cloud.handler
+(ns app.handler
+  (:gen-class)
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [compojure.handler :as handler]
+            [ring.adapter.jetty :as jetty]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (route/not-found "Not Found"))
 
-(def app
-  (wrap-defaults app-routes site-defaults))
+(def app 
+  (handler/site app-routes))
+
+(defn -main [& args]
+  (jetty/run-jetty app {:port 3000}))
