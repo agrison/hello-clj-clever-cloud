77cb7ab4af5cbe71e19a8773061f4a8a34114e58
Alexandre Grison <a.grison@gmail.com>
2016-06-24 09:09:39 +0200

    Add leiningen as dependency, set :source-paths and :test-paths

--- project.clj
+++ project.clj
@@ -4,9 +4,13 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.9.0-alpha5"]
                  [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]]
+                 [ring/ring-defaults "0.2.1"]
+                 [leiningen "2.6.1"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler hello-clj-clever-cloud.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+                        [ring/ring-mock "0.3.0"]]}}
+  :aot :all
+  :source-paths ["src/main/clojure"]
+  :test-paths ["src/test/clojure"])
